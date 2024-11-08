2c9c2058964e3cb9a2d88513a900ff6affc3cac3
Ratchanont Treevijitpaisan <r84389141@china.huawei.com>
2024-11-08 12:43:02 +0700

    module-vpc-1.2.0 add subnets default feature

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,8 +5,12 @@ resource "huaweicloud_vpc" "this" {
   for_each              = { for i, v in var.vpc : i => v }
   name                  = each.value.vpc_name
   cidr                  = each.value.vpc_cidr
+  secondary_cidrs       = each.value.vpc_cidr_secondary
   enterprise_project_id = each.value.vpc_enterprise_project
   tags                  = each.value.tags
+  region                = each.value.vpc_region
+  description           = each.value.vpc_description
+
 }
 ######################################################################
 # Associate Subnets according to VPCs creation
@@ -30,13 +34,22 @@ resource "huaweicloud_vpc_subnet" "this" {
 locals {
 
   vpc_subnets = flatten([
-    for vpc_index, vpc in var.vpc : [
+    for vpc_index, vpc in var.vpc : vpc.subnets != null ? [
       for subnet_index, subnet in vpc.subnets : {
         vpc_index      = vpc_index
         subnet_index   = subnet_index
         vpc_id         = huaweicloud_vpc.this[vpc_index].id
         subnet_cidr    = subnet.subnet_cidr
         subnet_name    = subnet.subnet_name
+        associate_tags = huaweicloud_vpc.this[vpc_index].tags
+      }
+      ] : [
+      {
+        vpc_index      = vpc_index
+        subnet_index   = 0
+        vpc_id         = huaweicloud_vpc.this[vpc_index].id
+        subnet_cidr    = cidrsubnet(vpc.vpc_cidr, 8, 1) # Default CIDR block
+        subnet_name    = "${vpc.vpc_name}-default-subnet"
         associate_tags = huaweicloud_vpc.this[vpc_index].tags
       }
     ]
